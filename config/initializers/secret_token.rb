5eceb6ba323c2ff48cc338da3d0a7f68e7ce7b13
helikopter <james@helikopterdesign.com>
2010-10-24 11:56:40 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Birdshit::Application.config.secret_token = '5adee8f7fc26817cbd890db8155073ccdc4703fc93e09e25de43f328b9a8ac26fab9562edca2dceb59c8088bf8f0d985274e69909cf4270884127f8f80985e99'
